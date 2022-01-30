f6b25262b656d6da8d1a177c6d038885a671bcd5
Kyosuke Kawamura <kawamura914914kyo@gmail.com>
2022-01-31 08:03:07 +0900

    init

---
+++ Sources/AccelParts/AccelParts.swift
@@ -0,0 +1,6 @@
+public struct AccelParts {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/AccelPartsTests/AccelPartsTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import AccelParts
+
+final class AccelPartsTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(AccelParts().text, "Hello, World!")
+    }
+}
