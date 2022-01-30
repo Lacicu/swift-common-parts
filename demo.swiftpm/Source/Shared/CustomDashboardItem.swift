4f89b8e7be2ad84c91c1079270f387e32228c90c
Kyosuke Kawamura <kawamura914914kyo@gmail.com>
2022-01-31 08:32:30 +0900

    make demo project

---
+++ demo.swiftpm/Source/Shared/CustomDashboardItem.swift
@@ -0,0 +1,20 @@
+
+import Foundation
+
+//public class CustomDashboardItem:  DashboardItem {
+//    public init() {}
+//    public var title: String = "Date"
+//    private var text: String = ""
+//    public func startMonitoring() {}
+//    public func stopMonitoring() {}
+//    public func update() {
+//        let formatter = DateFormatter()
+//        formatter.dateFormat = "HH:mm:ss"
+//        text = formatter.string(from: Date())
+//    }
+//    public var fetcher: MetricsFetcher {
+//        .text { [weak self] in
+//            self?.text ?? ""
+//        }
+//    }
+//}
---
+++ demo.swiftpm/Source/Shared/CustomDebugItem.swift
@@ -0,0 +1,23 @@
+//
+//  CustomDebugItem.swift
+//  Example
+//
+//  Created by Tomoya Hirano on 2020/12/06.
+//
+
+import UIKit
+//import DebugMenu
+//
+//public struct CustomDebugItem: DebugItem {
+//    public init() {}
+//    public let debugItemTitle: String = "Custom item"
+//    public let action: DebugItemAction = .toggle { UserDefaults.standard.bool(forKey: "key") } action: { (isOn, completions) in
+//        let userDefaults = UserDefaults.standard
+//        userDefaults.set(isOn, forKey: "key")
+//        if userDefaults.synchronize() {
+//            completions(.success(message: "Switch to \(isOn)"))
+//        } else {
+//            completions(.failure(message: "Failed to save"))
+//        }
+//    }
+//}
